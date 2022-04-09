f71b1fc5450d5fcee2f59c0650bb64dbb819f59d
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-04-09 06:54:45 -0300

    Feature (ColorContants): Make some colors

---
+++ DisneyPlusCloneApp/DisneyPlusCloneApp/Utils/ColorConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ColorConstants.swift
+//  DisneyPlusCloneApp
+//
+//  Created by Paolo Prodossimo Lopes on 09/04/22.
+//
+
+import SwiftUI
+
+struct ColorConstants {
+    
+    static let darkGrayColor: Color = .init(red: 0.050, green: 0.074, blue: 0.105)
+    static let darkBluishGrayColor: Color = .init(red: 0.125, green: 0.156, blue: 0.211)
+    
+}
