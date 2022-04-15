4926a2e72683ec9d6e3621538ace0bbb8eb4faa3
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-04-15 13:58:48 -0300

    Feature (CategoryAndDwonloa): Create this pages

--- DisneyPlusCloneApp/DisneyPlusCloneApp/Views/DownloadPageView.swift
+++ DisneyPlusCloneApp/DisneyPlusCloneApp/Views/DownloadPageView.swift
@@ -11,7 +11,24 @@ struct DownloadPageView: View {
     var body: some View {
         ZStack {
             GradientBackgroundView()
-        }
+            VStack {
+                Image(systemName: "square.and.arrow.down.fill")
+                    .font(.system(size: 60))
+                    .foregroundColor(.gray)
+                    .padding()
+                    .background(Circle().stroke(.gray, lineWidth: 4))
+                
+                Text("You have no downloads")
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.top, 10)
+                
+                Text("Movie and serie you download will \nappear here.")
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 5)
+            }
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
